403a5971f8733963d55869af1e5acb0d8d929891
orthemius <36065826+orthemius@users.noreply.github.com>
2022-11-21 12:35:48 +0700

    Automatic commit from Bubble

--- actions/AAa-850p2/client.js
+++ actions/AAa-850p2/client.js
@@ -1,12 +1,5 @@
 function(properties, context) {
     OneSignal.push(function(){
         OneSignal.setSubscription(false); 
-        //OneSignal.isPushNotificationsEnabled(function(isEnabled) {
-       // 	if (isEnabled) {
-       //         OneSignal.setSubscription(false);         
-      //      } else {
-          		//console.log("Push notifications are not enabled yet.");  
-       //     }
-      //  });
     });
 }
